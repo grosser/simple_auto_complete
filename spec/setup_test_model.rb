2dbf625659c4152e71814221a4918fcf608cc61f
grosser <grosser.michael@gmail.com>
2008-10-26 09:43:35 +0100

    more tests...

--- spec/setup_test_model.rb
+++ spec/setup_test_model.rb
@@ -5,6 +5,7 @@ require 'active_record/fixtures'
 #create model table
 ActiveRecord::Schema.define(:version => 1) do
   create_table "users" do |t|
+    t.string "full_name"
     t.string "name"
     t.timestamps
   end
--- spec/simple_autocomplete_spec.rb
+++ spec/simple_autocomplete_spec.rb
@@ -1,11 +1,29 @@
 require File.expand_path("spec_helper", File.dirname(__FILE__))
 
 describe UsersController do
+  before do
+    @c = UsersController.new
+    @c.stubs(:params).returns Hash.new
+    @c.stubs(:render)
+  end
+  
+  describe 'basics' do
+    it "can use long method/class names" do
+      class UserAddress < ActiveRecord::Base
+        set_table_name :users
+      end
+      UserAddress.expects(:find).with do |all,options|
+        options[:conditions] == ['LOWER(full_name) LIKE ?','%hans%']
+      end
+      
+      @c.stubs(:params).returns :q=>'Hans'
+      UsersController.autocomplete_for(:user_address,:full_name)
+      @c.autocomplete_for_user_address_full_name
+    end
+  end
+  
   describe 'simple autocomplete' do
     before do
-      @c = UsersController.new
-      @c.stubs(:params).returns Hash.new
-      @c.stubs(:render)
       UsersController.autocomplete_for(:user,:name)
     end
     
@@ -24,6 +42,30 @@ describe UsersController do
   end
   
   describe "autocomplete using blocks" do
+    it "evaluates the block" do
+      x=0
+      UsersController.autocomplete_for(:user,:name) do |items|
+        x=1
+      end
+      @c.autocomplete_for_user_name
+      x.should == 1
+    end
     
+    it "passes found items to the block" do
+      UsersController.autocomplete_for(:user,:name) do |items|
+        items.should == ['xx']
+      end
+      User.expects(:find).returns ['xx']
+      @c.autocomplete_for_user_name
+    end
+    
+    it "uses block output for render" do
+      UsersController.autocomplete_for(:user,:name) do |items|
+        items + 'xx'
+      end
+      User.expects(:find).returns 'aa'
+      @c.expects(:render).with {|hash| hash[:inline] == 'aaxx'}
+      @c.autocomplete_for_user_name
+    end
   end
 end
